e1eca9d4b107dfac5fdf61270d9ab51329a39a8e
mitchelinoue <mitchel_inoue@msn.com>
2022-10-20 17:58:21 -0300

    projeto sobre diretivas

--- diretivas/src/app/app.component.ts
+++ diretivas/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Produto } from './interfaces/produto';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'diretivas';
+  nome: string = '';
+  imagem: string = '';
+  preco: number = 0;
+  estoque: number = 0;
+  descricao: string = '';
+
+  produtos: Produto[] = [];
+
+  adicionarProduto(evento:any): void{
+    console.log(evento)
+    evento.preventDefault() //faz com que o comportamento padrão de um elemento HTML seja anulado
+    evento.stopPropagation() //evita com que a emissão do evento afete outros elementos HTML
+
+    this.produtos.push({
+      descricao: this.descricao,
+      estoque: this.estoque,
+      imagem: this.imagem,
+      nome: this.nome,
+      preco: this.preco,
+    })
+
+    this.nome = '';
+    this.imagem = '';
+    this.estoque = 0;
+    this.descricao = '';
+    this.preco = 0;
+  }
 }
