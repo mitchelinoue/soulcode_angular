a6e5053508a4396bf163dc48342336f4a2090356
mitchelinoue <mitchel_inoue@msn.com>
2022-10-25 17:55:19 -0300

    Angular Material e forms

--- pipes/src/app/string-cpf.pipe.ts
+++ pipes/src/app/string-cpf.pipe.ts
@@ -1,14 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Pipe({
   name: 'stringCpf',
 })
 export class StringCpfPipe implements PipeTransform {
   transform(value: string): string {
-    const identificacao = value.replace(/[^0-9]/g, '');
+    // 123.456.789-10
+    
 
-    return identificacao.replace(
-      /(\d{3})(\d{3})(\d{3})(\d{2})/g,'$1.$2.$3-$4'
-    );
+    //usando substring
+    const cpf = value.substring(0,3) + "." + value.substring(3,6) + "." + value.substring(6,9) + "-" + value.substring(9,11)
+
+    //usando regex
+    const cpf2 = value.replace(/([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{2})/,'$1.$2.$3-$4')
+
+    return cpf
+    
   }
 }
