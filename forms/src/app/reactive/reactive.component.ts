1a628528cac08678111a66fb6f9110e46fb1bb9f
mitchelinoue <mitchel_inoue@msn.com>
2022-10-27 17:40:59 -0300

    form e arrays

--- forms/src/app/reactive/reactive.component.ts
+++ forms/src/app/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -23,13 +23,49 @@ export class ReactiveComponent implements OnInit {
   /*
   FormGroup é uma estrutura do Reactive Forms que permite agruparmos forms controls dentro dela, para que o acesso aos valores seja mais fácil
    */
-  dadosForm: FormGroup = new FormGroup({
-    nome: new FormControl(''),
-    email: new FormControl(''),
-    senha: new FormControl('')
+
+  /* dadosForm: FormGroup = new FormGroup({
+    nome: new FormControl('', [ Validators.required, Validators.minLength(4) ]),
+    email: new FormControl('',[ Validators.required, Validators.email] ),
+    senha: new FormControl(''),
+    endereco: new FormGroup({
+      cep: new FormControl(''),
+      rua: new FormControl(''),
+      complemento: new FormControl(''),
+      numero: new FormControl('')
+    })
+  }) */
+
+  /*O método group é responsável por criar um novo Objeto do tipo FormGroup */
+
+  /*
+  FormArray é utilizado para armazenar form controls, form groups pu outros form arrays dentro de um array 
+  o FormArray precisa estar dentro de um FormGroup
+  */
+  dadosForm: FormGroup = this.fb.group({
+    nome: ['', [Validators.required, Validators.minLength(4)]],
+    email: ['', [Validators.required, Validators.email]],
+    senha: [''],
+    endereco: this.fb.group({
+      cep:[''],
+      rua:[''],
+      complemento:[''],
+      numero:['']
+    }),
+    telefones: this.fb.array([
+      [''] // pode utilizar 
+    ])
   })
 
-  constructor() { }
+  tels = this.dadosForm.get('telefones') as FormArray//precisa transformar o abstract control (resultado do 'get()')para form Array
+
+  /*
+  Form Builder -> Objeto que permite criar FormControls, FormGroups ou FormArrays com uma sintaxe menor
+   */
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
@@ -44,6 +80,10 @@ export class ReactiveComponent implements OnInit {
     console.log(this.senha1.value)
 
     console.log(this.dadosForm.value)
+  }
+
+  adicionarCampoDeTelefone(){
+    this.tels.push(new FormControl(''))
   }
 
 }
